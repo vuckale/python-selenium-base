42a2ea95e2875d5c82c0ec11178c79acf74c28fb
vuckale <56733276+vuckale@users.noreply.github.com>
2020-10-13 23:05:12 +0200

    added support for local storage and cookies

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager # pip install webdriver-manager
-
+import pickle
+import local_storage
+import os
+import itertools
+path = os.getcwd()
 class Bot:
 
     def __init__(self):
@@ -34,7 +38,54 @@ class Bot:
         # find correct version of chromedriver and install it
         self.driver = webdriver.Chrome(chrome_options = option, executable_path = ChromeDriverManager().install())
 
-        self.driver.get("https://web.whatsapp.com/")
+        self.driver.get("https://google.com/")
         
+    def get_storage(self):
+        '''get the local storage from current session'''
+        global path
+
+        # get the local storage
+        storage = local_storage.LocalStorage(self.driver)
+
+        with open('/local_storage', 'w') as f:
+            print(storage.items(), file=f)
+
+        with open(path+'/local_storage', 'w') as f:
+            for key, value in storage.items().items():
+                print(key + '\n' + value, file=f)
+
+
+    def load_storage_param(self, file):
+        '''load the data from @param file into local storage (must be parsed)'''
+        print("loading storage...")
+
+        # get the local storage
+        storage = local_storage.LocalStorage(self.driver)
+
+        with open(file) as f:
+            lines = [line.rstrip() for line in f]
+
+        local_storage_dict = dict(itertools.zip_longest(*[iter(lines)] * 2, fillvalue=""))
+
+        for key, value in local_storage_dict.items():
+            storage[key] = value
+
+        if len(local_storage_dict) > 0:
+            self.driver.refresh()
+
+        self.init_dir()
+
+
+    def get_cookies(self):
+	pickle.dump(self.driver.get_cookies(), open("cookies.pkl", "wb"))
+
+
+    def load_cookies(self):
+        cookies = pickle.load(open("cookies.pkl", "rb"))
+
+        for cookie in cookies:
+            print(cookies)
+            self.driver.add_cookie(cookie)
+
 b = Bot()
 
